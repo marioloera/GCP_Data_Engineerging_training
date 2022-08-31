8e553ba4c024fe7515f0e2a350b87454e1d83477
Mario Loera <marioll@kth.se>
2022-08-31 15:38:30 +0200

    create model draft

---
+++ Engineer Data in Google Cloud Challenge Lab/create_model.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE MODEL 
+  TODO
+TRANSFORM(
+)
+OPTIONS (
+  model_type='linear_reg',
+  labels = ['will_buy_on_return_visit']
+  -- OR input_label_cols=['label_col']
+)
+AS
+SELECT
+  TODO
+FROM 
+  TODO
