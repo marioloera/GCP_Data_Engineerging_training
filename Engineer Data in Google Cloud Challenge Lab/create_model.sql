92b9b1ce620b32878e0b6cb8c2321075d7aa8b7c
Mario Loera <marioll@kth.se>
2022-09-01 21:50:05 +0200

    Engineer Data in Google Cloud Challenge Lab COMPLETED

--- Engineer Data in Google Cloud Challenge Lab/create_model.sql
+++ Engineer Data in Google Cloud Challenge Lab/create_model.sql
@@ -1,14 +1,29 @@
 CREATE OR REPLACE MODEL 
-  TODO
+  `taxirides.fare_model_989`
 TRANSFORM(
+  ST_Distance(
+    ST_GeogPoint(pickuplon, pickuplat), 
+    ST_GeogPoint(dropofflon, dropofflat)
+  ) AS euclidean,
+  passengers,
+  # Use `ML.FEATURE_CROSS` function is a formed by multiplying two or more features. 
+  ML.FEATURE_CROSS(
+    STRUCT(
+      CAST(EXTRACT(DAYOFWEEK FROM pickup_datetime) AS STRING) AS dayofweek,
+      CAST(EXTRACT(HOUR FROM pickup_datetime) AS STRING) AS hourofday)
+  ) AS day_hr,
+  CONCAT( 
+    ML.BUCKETIZE(pickuplon, GENERATE_ARRAY(-78, -70, 0.01)),
+    ML.BUCKETIZE(pickuplat, GENERATE_ARRAY(37, 45, 0.01)),
+    ML.BUCKETIZE(dropofflon, GENERATE_ARRAY(-78, -70, 0.01)),
+    ML.BUCKETIZE(dropofflat, GENERATE_ARRAY(37, 45, 0.01)) 
+  ) AS pickup_and_dropoff,
+  fare_amount_898
 )
 OPTIONS (
   model_type='linear_reg',
-  labels = ['will_buy_on_return_visit']
-  -- OR input_label_cols=['label_col']
+  input_label_cols=['fare_amount_898']
 )
 AS
-SELECT
-  TODO
-FROM 
-  TODO
+SELECT *
+FROM `taxirides.taxi_training_data_806`
