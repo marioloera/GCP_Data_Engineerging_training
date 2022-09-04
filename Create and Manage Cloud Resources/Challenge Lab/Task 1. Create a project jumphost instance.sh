da3979006de5deb674b281d1e4e33357542c8f46
Mario Loera <marioll@kth.se>
2022-09-04 22:02:18 +0200

    fix task 1

--- Create and Manage Cloud Resources/Challenge Lab/Task 1. Create a project jumphost instance.sh
+++ Create and Manage Cloud Resources/Challenge Lab/Task 1. Create a project jumphost instance.sh
@@ -1,21 +1,4 @@
-MACHINE_NAME=www1
-MACHINE_TYPE=n1-standard-1
-ZONE=us-east1-b
+MACHINE_NAME=nucleus-jumphost-722
+MACHINE_TYPE=f1-micro
 
-gcloud compute instances create $MACHINE_NAME \
-    --machine-type $MACHINE_TYPE \
-    --zone $ZONE \
-    --tags http-server \
-    --network-interface=network-tier=PREMIUM,subnet=default
-
--- SSH connect
-gcloud compute ssh $MACHINE_NAME --zone $ZONE
-
--- Install an NGINX web server
-sudo apt-get update
-sudo apt-get install -y nginx
-ps auwx | grep nginx
-
-
--- To see the web page, return to the Cloud Console and click the External IP link in the row for your machine, 
--- or add the External IP value to http://EXTERNAL_IP/
+gcloud compute instances create $MACHINE_NAME --machine-type $MACHINE_TYPE
