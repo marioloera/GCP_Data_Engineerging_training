15432de997046fe40adaf1b7159319e0cc58a33f
Mario Loera <marioll@kth.se>
2022-09-04 21:30:48 +0200

    add file

--- Create and Manage Cloud Resources/Creating a Virtual Machine.sh
+++ Create and Manage Cloud Resources/Creating a Virtual Machine.sh
@@ -19,3 +19,19 @@ ps auwx | grep nginx
 
 -- To see the web page, return to the Cloud Console and click the External IP link in the row for your machine, 
 -- or add the External IP value to http://EXTERNAL_IP/
+
+# long command
+gcloud compute instances create gcelab4 \
+ --zone=us-west1-a \
+ --machine-type=e2-medium \
+ --network-interface=network-tier=PREMIUM,subnet=default \
+ --metadata=enable-oslogin=true \
+ --maintenance-policy=MIGRATE \
+ --provisioning-model=STANDARD \
+ --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+ --tags=http-server \
+ --create-disk=auto-delete=yes,boot=yes,device-name=gcelab3,image=projects/debian-cloud/global/images/debian-11-bullseye-v20220822,mode=rw,size=10,type=projects/qwiklabs-gcp-03-9a6b14c36b68/zones/us-west1-a/diskTypes/pd-balanced \
+ --no-shielded-secure-boot \
+ --shielded-vtpm \
+ --shielded-integrity-monitoring \
+ --reservation-affinity=any
