38eb49afa39cf0e53200aeca68dbf227b26d19a2
Mario Loera <marioll@kth.se>
2022-09-12 07:20:42 +0200

    TASK  5 & 6

--- Build and Optimize Data Warehouses with BigQuery/challenge_lab/dev.sql
+++ Build and Optimize Data Warehouses with BigQuery/challenge_lab/dev.sql
@@ -54,3 +54,45 @@ SCHEMA_FILE=schema_oxford_policy_tracker.json
     SET t0.country_area = t2.country_area
     FROM `bigquery-public-data.census_bureau_international.country_names_area` AS t2
     WHERE t0.country_name = t2.country_name;
+
+-- Task 5. Populate the mobility record data
+    UPDATE `covid_841.oxford_policy_tracker_549` AS t0
+    SET
+      t0.mobility.avg_retail = t2.avg_retail,
+      t0.mobility.avg_grocery = t2.avg_grocery,
+      t0.mobility.avg_parks = t2.avg_parks,
+      t0.mobility.avg_transit = t2.avg_transit,
+      t0.mobility.avg_workplace = t2.avg_workplace,
+      t0.mobility.avg_residential = t2.avg_residential
+    FROM (
+      SELECT
+        country_region,
+        AVG(retail_and_recreation_percent_change_from_baseline) AS avg_retail, 
+        AVG(grocery_and_pharmacy_percent_change_from_baseline) AS avg_grocery, 
+        AVG(parks_percent_change_from_baseline) AS avg_parks, 
+        AVG(transit_stations_percent_change_from_baseline) AS avg_transit, 
+        AVG(workplaces_percent_change_from_baseline) AS avg_workplace,
+        AVG(residential_percent_change_from_baseline) AS avg_residential  
+      FROM `bigquery-public-data.covid19_google_mobility.mobility_report`
+      GROUP BY 1
+    ) AS t2
+    WHERE t0.country_name = t2.country_region;
+
+-- Task 6. Query missing data in population & country_area columns
+    /*
+    DISTINCT countries that do not have any population data
+        and countries that do not have country area information
+    ordered by country name. 
+    If a country has neither population nor country area it should appear twice. 
+    This will give you an idea of problematic countries.
+    */
+
+    SELECT DISTINCT country_name
+    FROM `covid_841.oxford_policy_tracker_549` 
+    WHERE population IS NULL
+
+    UNION ALL
+    SELECT DISTINCT country_name
+    FROM `covid_841.oxford_policy_tracker_549` 
+    WHERE country_area IS NULL
+    ORDER BY 1
