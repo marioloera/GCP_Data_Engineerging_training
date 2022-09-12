14e123740141d390a75f0da311218cb59e5bb5c0
Mario Loera <marioll@kth.se>
2022-09-12 06:38:46 +0200

    fix

--- Build and Optimize Data Warehouses with BigQuery/challenge_lab/dev.sql
+++ Build and Optimize Data Warehouses with BigQuery/challenge_lab/dev.sql
@@ -1,13 +1,13 @@
 -- replace
-DATASET_ID=covid_983
-TABLE_ID=oxford_policy_tracker_791
+DATASET_ID=covid_841
+TABLE_ID=oxford_policy_tracker_549
 SCHEMA_FILE=schema_oxford_policy_tracker.json
 
 -- Task 0. Create dataset
 
 -- Task 1. Create a table partitioned by date
     -- TEMP TABLE
-    CREATE OR REPLACE TABLE covid_983.oxford_policy_tracker_temp
+    CREATE OR REPLACE TABLE covid_841.oxford_policy_tracker_temp
     AS
         SELECT *
         FROM `bigquery-public-data.covid19_govt_response.oxford_policy_tracker`
@@ -15,12 +15,12 @@ SCHEMA_FILE=schema_oxford_policy_tracker.json
             AND alpha_3_code NOT IN ('BRA', 'CAN', 'GBR', 'USA')
 
     -- UPDATE date to be under 360 days
-    UPDATE `covid_983.oxford_policy_tracker_temp` AS t
-    SET t0.date = DATE_ADD(t.date, INTERVAL 1 YEAR)
+    UPDATE `covid_841.oxford_policy_tracker_temp` AS t
+    SET t.date = DATE_ADD(t.date, INTERVAL 1 YEAR)
     WHERE TRUE
 
     -- create target table
-    CREATE OR REPLACE TABLE covid_983.oxford_policy_tracker_791
+    CREATE OR REPLACE TABLE covid_841.oxford_policy_tracker_549
         PARTITION BY date
         OPTIONS(
             partition_expiration_days=360
@@ -38,7 +38,6 @@ SCHEMA_FILE=schema_oxford_policy_tracker.json
 
     -- manually add the fields in the extra_fields.json
 
-    bq update $DATASET_ID.$TABLE_ID > $SCHEMA_FILE
+    bq update $DATASET_ID.$TABLE_ID $SCHEMA_FILE
 
-
-
+-- Task 3. Add country population data to the population column
